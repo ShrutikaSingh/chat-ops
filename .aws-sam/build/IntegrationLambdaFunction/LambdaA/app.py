2dfc505144f654309406cfa48b438235987597a9
ShrutikaSingh <shrutika051220@gmail.com>
2024-06-17 14:16:13 -0700

    remove .aws-sam from git tracking

---
+++ .aws-sam/build/IntegrationLambdaFunction/LambdaA/app.py
@@ -0,0 +1,26 @@
+# LambdaA/app.py
+
+import json
+import boto3
+
+def lambda_handler(event, context):
+    print('LambdaA received event:', json.dumps(event))
+
+    # Invoke LambdaB
+    client = boto3.client('lambda', endpoint_url='http://localhost:3001', region_name='us-east-1')
+
+    try:
+        response = client.invoke(
+            FunctionName='LambdaB',
+            InvocationType='RequestResponse',
+            Payload=json.dumps(event)
+        )
+        data = json.loads(response['Payload'].read().decode('utf-8'))
+        print('LambdaB invocation result:', data)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(data)
+        }
+    except Exception as e:
+        print('Error invoking LambdaB:', e)
+        raise e
